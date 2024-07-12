e3ea8a4840712f896aaced0ca2f129d453c0b923
Léo Nguyen <72701625+Vexus95@users.noreply.github.com>
2024-07-12 12:52:24 +0200

    DELETE A

--- StoryTeller/app/test.py
+++ StoryTeller/app/test.py
@@ -1,16 +1,1 @@
-from openai import OpenAI
-client = OpenAI(
-    api_key = "sk-proj-vRTvUjzr3aZXwZRhYBMvT3BlbkFJZW9yFMGhCbw5dNJKFNZQ"
-)
-
-
-response = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "You are a storyteller."},
-        {"role": "user", "content": "Once upon a time in a land far, far away..."}
-    ],
-)
-
-print(response.choices[0].message)
 
